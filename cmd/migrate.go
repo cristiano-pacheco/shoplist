b9f2949cae242b4f1d8e659f5de155a59aa9c2a8
Cristiano Pacheco <chris.spb25@gmail.com>
2024-10-08 21:56:23 -0300

    feat: moves the dbmigration service to run directly on the migrate command

--- cmd/migrate.go
+++ cmd/migrate.go
@@ -1,10 +1,15 @@
 package cmd
 
 import (
-	"github.com/cristiano-pacheco/go-modulith/internal/module/dbmigration"
+	"fmt"
+	"log"
+	"os"
+
 	"github.com/cristiano-pacheco/go-modulith/internal/shared/config"
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 	"github.com/spf13/cobra"
-	"go.uber.org/fx"
 )
 
 // dbMigrateCmd represents the migrate command
@@ -13,11 +18,21 @@ var dbMigrateCmd = &cobra.Command{
 	Short: "Run database migrations",
 	Long:  `Run database migrations. This command will run all the migrations that have not been run yet.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		app := fx.New(
-			config.Module,
-			dbmigration.Module,
-		)
-		app.Run()
+		config.Init()
+		cfg := config.GetConfig()
+		dsn := config.GeneratePostgresDatabaseDSN(cfg)
+
+		m, err := migrate.New("file://migrations", dsn)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if err := m.Up(); err != nil && err != migrate.ErrNoChange {
+			log.Fatal(err)
+		}
+
+		fmt.Println("Migrations executed successfully")
+		os.Exit(0)
 	},
 }
 
--- internal/module/dbmigration/module.go
+++
@@ -1,8 +0,0 @@
-package dbmigration
-
-import (
-	"github.com/cristiano-pacheco/go-modulith/internal/module/dbmigration/service"
-	"go.uber.org/fx"
-)
-
-var Module = fx.Module("dbmigration", fx.Invoke(service.ExecuteMigrations))
